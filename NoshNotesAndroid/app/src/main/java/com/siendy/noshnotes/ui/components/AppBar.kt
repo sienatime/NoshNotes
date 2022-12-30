275b440f7627ab5edcde6529fd16b14f98d1e513
Siena Aguayo <siena.aguayo@gmail.com>
2022-12-29 19:13:03 -0800

    add fab

--- NoshNotesAndroid/app/src/main/java/com/siendy/noshnotes/ui/components/AppBar.kt
+++ NoshNotesAndroid/app/src/main/java/com/siendy/noshnotes/ui/components/AppBar.kt
@@ -3,14 +3,19 @@ package com.siendy.noshnotes.ui.components
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.siendy.noshnotes.R.string
+import com.siendy.noshnotes.R
 import com.siendy.noshnotes.ui.theme.NoshNotesTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,20 +23,5 @@ import com.siendy.noshnotes.ui.theme.NoshNotesTheme
 fun AppBarScreen(
   content: @Composable () -> Unit
 ) {
-  NoshNotesTheme {
-    Scaffold(
-      topBar = {
-        TopAppBar(
-          title = { Text(stringResource(id = string.app_name)) }
-        )
-      },
-      content = {
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colorScheme.background,
-          content = content
-        )
-      }
-    )
-  }
+
 }
--- NoshNotesAndroid/app/src/main/java/com/siendy/noshnotes/ui/screens/MainScreen.kt
+++ NoshNotesAndroid/app/src/main/java/com/siendy/noshnotes/ui/screens/MainScreen.kt
@@ -1,9 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.siendy.noshnotes.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -12,8 +25,8 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.siendy.noshnotes.R
-import com.siendy.noshnotes.ui.components.AppBarScreen
 import com.siendy.noshnotes.ui.components.TabBar
+import com.siendy.noshnotes.ui.theme.NoshNotesTheme
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -26,29 +39,52 @@ fun MainScreen() {
     stringResource(R.string.map)
   )
 
-  AppBarScreen {
-    Column(
-      modifier = Modifier.fillMaxHeight()
-    ) {
-      Column(
-        modifier = Modifier
-          .fillMaxWidth()
-          .weight(1f)
-      ) {
-        Text(text = "helloooooo")
-
-        HorizontalPager(
-          count = pages.size,
-          state = pagerState
-        ) { page ->
-          when (page) {
-            0 -> PlacesList()
-            1 -> PlacesMap()
-          }
+  NoshNotesTheme {
+    Scaffold(
+      topBar = {
+        TopAppBar(
+          title = { Text(stringResource(id = R.string.app_name)) }
+        )
+      },
+      floatingActionButtonPosition = FabPosition.End,
+      floatingActionButton = {
+        FloatingActionButton(
+          onClick = {}
+        ) {
+          Icon(Filled.Add, stringResource(id = R.string.add_place))
         }
-      }
+      },
+      content = {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background,
+          content = {
+            Column(
+              modifier = Modifier.fillMaxHeight()
+            ) {
+              Column(
+                modifier = Modifier
+                  .fillMaxWidth()
+                  .weight(1f)
+              ) {
+                Text(text = "helloooooo")
 
-      TabBar(pages, pagerState)
-    }
+                HorizontalPager(
+                  count = pages.size,
+                  state = pagerState
+                ) { page ->
+                  when (page) {
+                    0 -> PlacesList()
+                    1 -> PlacesMap()
+                  }
+                }
+              }
+
+              TabBar(pages, pagerState)
+            }
+          }
+        )
+      }
+    )
   }
 }
