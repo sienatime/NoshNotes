834fd2e58dd240fd09e7ece27b36e6c209b63e87
Andy Bartholomew <andy.bartholomew@airbnb.com>
2023-01-16 11:57:26 -0800

    create new project and fix indentation

---
+++ NoshNotesIOS/NoshNotes/NoshNotes/NoshNotesApp.swift
@@ -0,0 +1,13 @@
+// Created by andy_bartholomew on 1/16/23.
+// Copyright © 2023 Airbnb Inc. All rights reserved.
+
+import SwiftUI
+
+@main
+struct NoshNotesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
