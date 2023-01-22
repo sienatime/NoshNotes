3b3ea18addb36737c72f9fed6c9ea35ff9dc8366
Andy Bartholomew <andy.bartholomew@airbnb.com>
2023-01-22 15:12:58 -0800

    add app delegate setup

--- NoshNotesIOS/NoshNotes/NoshNotes/AppDelegate.swift
+++ NoshNotesIOS/NoshNotes/NoshNotes/AppDelegate.swift
@@ -1,9 +1,10 @@
 // Created by andy_bartholomew on 1/16/23.
 // Copyright © 2023 Airbnb Inc. All rights reserved.
 
-import Foundation
-import SwiftUI
 import FirebaseCore
+import Foundation
+import GoogleMaps
+import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(
@@ -11,6 +12,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
   {
     FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyBOpFP6JvDulE8Qf4B17yODOnWifA4bj2Q")
+//    GMSPlacesClient.provideAPIKey("AIzaSyBOpFP6JvDulE8Qf4B17yODOnWifA4bj2Q")
 
     return true
   }
